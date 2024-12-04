6d2238076d02812fd96084faf0ec3f650264e803
Troy Benson <troybensonsa@gmail.com>
2024-12-04 23:21:08 +0000

    add features

--- crates/postcompile/src/lib.rs
+++ crates/postcompile/src/lib.rs
@@ -95,13 +95,17 @@ pub fn compile_custom(tokens: &[u8], config: &Config) -> Result<CompileOutput, E
 
     let output = child.wait_with_output().unwrap();
 
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    #[cfg(feature = "prettyplease")]
+    let stdout = syn::parse_file(&stdout).map(|file| prettyplease::unparse(&file)).unwrap_or(stdout);
+
     Ok(CompileOutput {
         status: if output.status.success() {
             ExitStatus::Success
         } else {
             ExitStatus::Failure(output.status.code().unwrap_or(-1))
         },
-        stdout: String::from_utf8(output.stdout).unwrap(),
+        stdout,
         stderr: String::from_utf8(output.stderr).unwrap(),
     })
 }
